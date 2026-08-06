807660cbe36c9e814f8d86bc9f82e3af1895644c
dendagzj <danideandaguzman@gmail.com>
2026-08-06 20:23:44 +0000

    jeraquias en limites.py

--- limites.py
+++ limites.py
@@ -17,6 +17,8 @@ with open("json/entrenamiento/dataset.json", "r", encoding="utf-8") as f:
     dataset = json.load(f)
 
 
+
+# ========== Funciones ==========
 def contar_tokens():
     """
     Cuenta la cantidad total de tokens del dataset.
@@ -35,23 +37,48 @@ def contar_tokens():
 
 def eliminar_mensajes():
     """
-    Elimina mensajes con más de 500 tokens hasta que el dataset
-    quede por debajo del límite establecido.
+    Elimina mensajes con más segun la sigientes jerarquias:
+    0.9 se dejaran solo los mensajes que se repiten mas de 3 veces
+    0.6 se dejan los que son menores a 500 tokens
+    0.3 se dejan los que son negativos
+
+    esta jerarqia ayuda a optimizar los mensajes de manera mas sensilla ademas
+    que al usar un algoritmo de optimizacion en vez de un modelo de ai se
+    optimiza el proceso.
     """
     global dataset
 
-    # Recorrer una copia para poder eliminar elementos sin problemas
+    # Recorrer una copia para poder eliminar elementos mayores a 500 tokens sin afectar la iteración
     for mensaje in dataset[:]:
 
         tokens = tokenizer(mensaje["text"], return_tensors="pt")
         cantidad_tokens = tokens["input_ids"].size(1)
+        mensajes_eliminados = 0
 
-        if cantidad_tokens > 500:
+        if cantidad_tokens > 500 and mensaje["qualification"] == "positive": # hace que si en el valor qualification es positive y es superior a 500 tokens se elimine el mensaje pero si es negative no se elimine
 
             print(f"Eliminando mensaje ({cantidad_tokens} tokens)")
             print(mensaje["text"])
             print("-" * 60)
 
+            dataset.remove(mensaje)
+            mensajes_eliminados += 1
+            # Recalcular el total
+            contar_tokens()
+
+            if tokens_actuales <= limite_tokens:
+                break
+            elif mensajes_eliminados >= 1_000:  # Limitar a 10 eliminaciones por ejecución
+                break
+    
+    # buscar mensajes que se repiten menos de 3 veses y eliminarlos
+    mensaje_contador = 0
+    for mensaje in dataset:
+        if dataset.count(mensaje) < 3 and mensaje["qualification"] == "positive": # hace que si en el valor qualification es positive y se repite menos de 3 veces se elimine el mensaje pero si es negative no se elimine
+            mensaje_contador += 1
+            print(f"Eliminando mensaje que se repite menos de 3 veces ({mensaje_contador})")
+            print(mensaje["text"])
+            print("-" * 60)
             dataset.remove(mensaje)
 
             # Recalcular el total
@@ -59,13 +86,18 @@ def eliminar_mensajes():
 
             if tokens_actuales <= limite_tokens:
                 break
+            elif mensaje_contador >= 1_000:  # Limitar a 10 eliminaciones por ejecución
+                break
+
+
 
     # Guardar el dataset actualizado
     with open("json/entrenamiento/dataset.json", "w", encoding="utf-8") as f:
         json.dump(dataset, f, ensure_ascii=False, indent=4)
 
-    print("Dataset actualizado correctamente.")
 
+
+    
 
 def limites_tokens():
     """
